6c885d86a65d368bad5199ba3d5aec282fb57a93
tanahiro2010 <herentongkegu087@gmail.com>
2026-09-21 17:27:43 +0900

    feat(profile): profileの追加コマンドを作成

---
+++ src/presentation/cli/commands/profile/add.ts
@@ -0,0 +1,23 @@
+import { Command } from "commander";
+import { Config } from "../../../../infrastructure/config";
+import CommandBase from "../base";
+
+
+class ProfileAddCommand extends CommandBase {
+  protected readonly name = "add";
+  protected readonly description = "add growi profile";
+
+  protected configure(command: Command): void {
+    command
+      .argument("<profileId>", "Id of profile")
+      .argument("<accessToken>", "Growi access token")
+      .argument("<baseEndpoint>", "Growi base endpoint");
+  }
+
+  protected async action(profileId: string, accessToken: string, baseEndpoint: string): Promise<void> {
+    Config.profiles.add(profileId, accessToken, baseEndpoint);
+    console.log(`Profile '${profileId}' added`);
+  }
+}
+
+export { ProfileAddCommand }
